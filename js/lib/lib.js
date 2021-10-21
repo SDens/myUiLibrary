2387251dc58b2ef69c7231eca8d6d86c2db12119
SDens <ssd27@mail.ru>
2021-10-22 00:54:50 +0300

    SDens
    MyUiLibrary

---
+++ js/lib/lib.js
@@ -0,0 +1,16 @@
+import $ from './core';
+import './modules/display';
+import './modules/classes';
+import './modules/hendlers';
+import './modules/actions';
+import './modules/effects';
+import './modules/effects';
+import './components/dropdown';
+import './components/modal';
+import './components/tab';
+import './components/accordeon';
+import './components/carousel';
+import './services/requests';
+
+
+export default $;
